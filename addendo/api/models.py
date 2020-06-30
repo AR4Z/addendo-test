9c467ec48821bd54d993d8d571bad4ccccb60e1b
Orlando Hernandez <ohernandezn@unal.edu.co>
2020-06-30 11:02:54 -0500

    Create Consultant and Organization models

--- addendo/api/models.py
+++ addendo/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Organization(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+class Consultant(models.Model):
+    name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
