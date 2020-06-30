3ed4f3de3d876bdc498611dd5a436797665774c2
Orlando Hernandez <ohernandezn@unal.edu.co>
2020-06-30 11:29:48 -0500

    Add views

--- addendo/api/views.py
+++ addendo/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Consultant, Organization
+from rest_framework import viewsets
+from .serializers import ConsultantSerializer, OrganizationSerializer
 
-# Create your views here.
+class ConsultantViewSet(viewsets.ModelViewSet):
+    queryset = Consultant.objects.all()
+    serializer_class = ConsultantSerializer
+
+class OrganizationViewSet(viewsets.ModelViewSet):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
